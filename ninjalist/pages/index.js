59e8a3c209acff34c2209bfe22e3c6c179f5fc2d
Mario Lazzari <mario.lazzari@italcoeng.com>
2021-01-26 14:50:14 +0100

    Done lesson 3

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Navbar from "../comps/Navbar";
+import Footer from "../comps/Footer";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
+
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam alias,
@@ -15,6 +19,7 @@ export default function Home() {
         dolorem aspernatur provident odit expedita minus fugit consequuntur
         maxime eos?
       </p>
+      <Footer />
     </div>
   );
 }
