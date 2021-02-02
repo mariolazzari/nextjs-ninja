55e10f10509b55a4911d9652b0500b73236134e4
Mario Lazzari <mario.lazzari@gmail.com>
2021-02-02 20:05:11 +0100

    Done lesson 10

--- ninjalist/pages/ninjas/index.js
+++ ninjalist/pages/ninjas/index.js
@@ -1,9 +1,29 @@
-const Ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
 };
 
 export default Ninjas;
